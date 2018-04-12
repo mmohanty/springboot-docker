aba038a4ed4d990b801673433377ed73a3fa4fe2
manasmohanty <manasmohanty07@gmail.com>
2018-04-12 21:41:49 +0530

    adding src file

---
+++ src/main/java/org/manas/spring/docker/App.java
@@ -0,0 +1,15 @@
+package org.manas.spring.docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class App {
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+}
